2b3ebb583f166380766b63d0886c60a00996de2d
Anderson Danilo <contact@andersondanilo.com>
2022-12-09 21:49:24 -0300

    fix: picom start

--- awesomewm/.config/awesome/custom/autostart.lua
+++ awesomewm/.config/awesome/custom/autostart.lua
@@ -1,6 +1,6 @@
 local awful = require("awful")
 
 -- autostart
-awful.spawn.with_shell("picom --experimental-backends --log-file=/tmp/picom.log --log-level=warn")
+awful.spawn.with_shell("picom --log-file=/tmp/picom.log --log-level=warn")
 awful.spawn.with_shell("nm-applet")
 awful.spawn.with_shell("blueman-applet")
